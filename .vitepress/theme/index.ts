88762bccd81e578abfae43088370d8e53f6613b0
yuki <dc_yuki@qq.com>
2022-07-03 21:57:15 +0800

    update v0.4 doc

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,13 @@
+import DefaultTheme from 'vitepress/theme';
+import ChatAvatar from './components/ChatAvatar.vue';
+import ChatMessage from './components/ChatMessage.vue';
+import ChatPanel from './components/ChatPanel.vue';
+
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('ChatAvatar', ChatAvatar);
+    app.component("ChatMessage", ChatMessage);
+    app.component("ChatPanel", ChatPanel);
+  }
+}
