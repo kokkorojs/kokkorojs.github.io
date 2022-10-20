7b935bd8a83be4c2a3927dcdbc62918b721e9d39
Yuki <admin@yuki.sh>
2022-10-20 21:37:29 +0800

    feat: add theme components

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,15 @@
 import DefaultTheme from 'vitepress/theme';
-import ChatAvatar from './components/ChatAvatar.vue';
-import ChatMessage from './components/ChatMessage.vue';
-import ChatPanel from './components/ChatPanel.vue';
+
+import Layout from './components/theme/Layout.vue';
+import NotFound from './components/theme/NotFound.vue';
+import ChatPanel from './components/chat/ChatPanel.vue';
+import ChatAvatar from './components/chat/ChatAvatar.vue';
+import ChatMessage from './components/chat/ChatMessage.vue';
 
 export default {
-  ...DefaultTheme,
+  // ...DefaultTheme,
+  Layout,
+  NotFound,
   enhanceApp({ app }) {
     app.component('ChatAvatar', ChatAvatar);
     app.component("ChatMessage", ChatMessage);
