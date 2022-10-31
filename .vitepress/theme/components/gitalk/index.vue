6cfa4929848d8888cc4b30b7a2d76c6e5c40e596
Yuki <admin@yuki.sh>
2022-10-31 17:38:59 +0800

    feat: add gitalk

---
+++ .vitepress/theme/components/gitalk/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import 'gitalk/dist/gitalk.css';
+import Gitalk from 'gitalk';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const gitalk = new Gitalk({
+    clientID: '2966b08bee02e561b5f0',
+    clientSecret: '717c72176ebe736b21274c4821a2ee23a8042b73',
+    repo: 'kokkorojs.github.io',
+    owner: 'dcyuki',
+    admin: ['dcyuki'],
+    id: location.pathname,
+    distractionFreeMode: false,
+  });
+  gitalk.render('gitalk-container');
+});
+</script>
+
+<template>
+  <div id="gitalk-container"></div>
+</template>
+
+<style lang="scss">
+
+</style>
