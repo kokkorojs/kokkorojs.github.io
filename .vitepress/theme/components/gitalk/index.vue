932ac649e6710411dbbb539667e15cccb64f06ff
Yuki <admin@yuki.sh>
2023-02-25 17:04:25 +0800

    feat: improved gitalk

--- .vitepress/theme/components/gitalk/index.vue
+++ .vitepress/theme/components/gitalk/index.vue
@@ -2,8 +2,9 @@
 import 'gitalk/dist/gitalk.css';
 import Gitalk from 'gitalk';
 import { onMounted, watch, ref } from 'vue';
-import { useRoute } from 'vitepress';
+import { useRoute, useData } from 'vitepress';
 
+const data = useData();
 const route = useRoute();
 const gitalkRef = ref<HTMLElement>();
 const emptyNode = (node: HTMLElement): void => {
@@ -14,6 +15,9 @@ const emptyNode = (node: HTMLElement): void => {
 const initGitalk = (): void => {
   emptyNode(gitalkRef.value!);
 
+  if (location.pathname === '/' || data.frontmatter.value.comments === false) {
+    return;
+  }
   const gitalk = new Gitalk({
     clientID: '2966b08bee02e561b5f0',
     clientSecret: '717c72176ebe736b21274c4821a2ee23a8042b73',
